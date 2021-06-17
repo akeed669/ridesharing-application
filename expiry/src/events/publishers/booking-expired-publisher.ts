8bb962c76299e0f292f78a4b3843f0f74af47cb1
Akeed Manuideen <akeedmanuideen@gmail.com>
2021-06-17 17:17:59 +0530

    deleted old expiry service

--- expiry/src/events/publishers/booking-expired-publisher.ts
+++
@@ -1,5 +0,0 @@
-import { Publisher, BookingExpiredEvent, Subjects } from "@orgakeed/commons";
-
-export class BookingExpiredPublisher extends Publisher<BookingExpiredEvent> {
-  subject: Subjects.BookingExpired = Subjects.BookingExpired;
-}
